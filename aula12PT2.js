bacb69817add3cb446202acdf506aec8445fe200
Matheus kikuti sousa <111128991+KikuTiii@users.noreply.github.com>
2022-12-28 14:02:32 -0300

    Add files via upload

---
+++ aula12PT2.js
@@ -0,0 +1,8 @@
+ var idade = 67
+ if (idade < 18){
+    console.log('Não vota')
+ }else if(idade < 18 || idade > 65){
+    console.log ('voto opcional')
+ }else if(idade >=18){
+   console.log('voto obrigatorio')
+ }
